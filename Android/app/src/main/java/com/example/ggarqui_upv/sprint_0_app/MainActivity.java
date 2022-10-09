b8bf739003d6a2c606fde5a686cbd3d8ac4bf7dc
Zero33w <guillem46723@gmail.com>
2022-10-10 00:07:31 +0200

    ahora la id de las medidas es autoincremental y se captura y pasa el momento de la medición

--- Android/app/src/main/java/com/example/ggarqui_upv/sprint_0_app/MainActivity.java
+++ Android/app/src/main/java/com/example/ggarqui_upv/sprint_0_app/MainActivity.java
@@ -21,12 +21,16 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TimeZone;
 
 public class MainActivity extends AppCompatActivity {
 
-    //creamos objetos que usaremos para interactuar con los elementos del layout
+    //declaramos objetos que usaremos para interactuar con los elementos del layout
     EditText edtIdMedicion,edtIdSensor,edtValorMedicion;
     Button btnAgregar, btnBuscar;
 
@@ -36,26 +40,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Vinculamos los objetos creadas con los elementos del layout
+        //Vinculamos los objetos creados con los elementos del layout
         edtIdMedicion=(EditText) findViewById(R.id.edtIdMedicion);
         edtIdSensor=(EditText) findViewById(R.id.edtIdSensor);
         edtValorMedicion=(EditText) findViewById(R.id.edtValorMedicion);
         btnAgregar=(Button) findViewById(R.id.btnAgregar);
         btnBuscar=(Button) findViewById(R.id.btnBuscar);
 
+
         //el boton agregar tras ser clicado realiza la llamada a la funcion ejecutarServicio
         btnAgregar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ejecutarServicio("http://10.236.27.138:80/sprint0/insertar_medida.php");
+                ejecutarServicio("http://192.168.1.141:80/sprint0/insertar_medida.php");
                 //ejecutarServicio("http://192.168.1.153:80/sprint0/insertar_medida.php");
             }
         });
 
+        //el boton buscar tras ser clicado realiza la llamada a la funcion buscarMedida
         btnBuscar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                buscarMedida("http://10.236.27.138:80/sprint0/buscar_medida.php?idMedicion="+edtIdMedicion.getText()+"");
+                buscarMedida("http://192.168.1.141:80/sprint0/buscar_medida.php?idMedicion="+edtIdMedicion.getText()+"");
                // buscarMedida("http://192.168.1.153:80/sprint0/buscar_medida.php?idMedicion="+edtIdMedicion.getText()+"");
             }
         });
@@ -63,6 +69,7 @@ public class MainActivity extends AppCompatActivity {
 
     //La siguiente funcion realiza la consulta para subir los datos en los distintos campos del layout como una nueva entrada en la base de datos
     protected void ejecutarServicio(String enlace){
+
         StringRequest stringRequest=new StringRequest(Request.Method.POST, enlace, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
@@ -80,6 +87,7 @@ public class MainActivity extends AppCompatActivity {
                 parametros.put("idMedicion",edtIdMedicion.getText().toString());
                 parametros.put("idSensor",edtIdSensor.getText().toString());
                 parametros.put("valorMedicion",edtValorMedicion.getText().toString());
+                parametros.put("momentoMedicion",momentoActual());
                 return parametros;
             }
         };
@@ -112,5 +120,15 @@ public class MainActivity extends AppCompatActivity {
         );
         requestQueue=Volley.newRequestQueue(this);
         requestQueue.add(jsonArrayRequest);
+    }
+
+    //La siguiente funcion sirve para capturar la fecha y hora en formato timestamp del momento en que es llamada
+    public String momentoActual(){
+        TimeZone tz = TimeZone.getTimeZone("GMT+2");
+        Calendar calendar= Calendar.getInstance(tz);
+        SimpleDateFormat simpleDateFormat= new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        simpleDateFormat.setTimeZone(tz);
+        String momento=simpleDateFormat.format(calendar.getTime());
+        return momento;
     }
 }
